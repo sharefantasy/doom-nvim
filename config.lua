ca999093bacff44263733038081eb6ec68be1b8e
fanqineng <fanqineng@bytedance.com>
2023-10-08 15:06:57 +0800

    1
    Change-Id: I01759ad7f3fe47d51d4515a4e9121dbdeeeda397

--- config.lua
+++ config.lua
@@ -538,6 +538,38 @@ doom.use_package({
 
 doom.use_package({'kevinhwang91/nvim-bqf', ft = 'qf'})
 
+doom.use_package( {
+  'nvim-orgmode/orgmode',
+  dependencies = {
+    { 'nvim-treesitter/nvim-treesitter', lazy = true },
+  },
+  event = 'VeryLazy',
+  config = function()
+    -- Load treesitter grammar for org
+    require('orgmode').setup_ts_grammar()
+
+    -- Setup treesitter
+    require('nvim-treesitter.configs').setup({
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { 'org' },
+      },
+      -- ensure_installed = { 'org' },
+    })
+
+    -- Setup orgmode
+    require('orgmode').setup({
+      org_agenda_files = '~/orgfiles/**/*',
+      org_default_notes_file = '~/orgfiles/refile.org',
+    })
+  end,
+})
+doom.use_package({
+        'lukas-reineke/headlines.nvim',
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        config = true, -- or `opts = {}`
+    })
+
 -- doom.use_package( {
 --     'nvimdev/lspsaga.nvim',
 --     config = function()
